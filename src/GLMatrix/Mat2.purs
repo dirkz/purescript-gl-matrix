4f157d963958c4d0745cafdb729b0944a1d803de
Dirk Zimmermann <dirk@pep-project.org>
2020-08-14 11:17:59 +0200

    Mat2.add

--- src/GLMatrix/Mat2.purs
+++ src/GLMatrix/Mat2.purs
@@ -1,6 +1,6 @@
 module GLMatrix.Mat2 where
 
-import Data.Function.Uncurried (Fn4, runFn4)
+import Data.Function.Uncurried (Fn2, Fn4, runFn2, runFn4)
   
 foreign import data Mat2 :: Type
 
@@ -9,3 +9,9 @@ foreign import js_fromValues :: Fn4 Number Number Number Number Mat2
 -- |Create a new mat2 with the given values
 fromValues :: Number -> Number -> Number -> Number -> Mat2
 fromValues = runFn4 js_fromValues
+
+foreign import js_add :: Fn2 Mat2 Mat2 Mat2
+
+-- |Adds two mat2's
+add :: Mat2 -> Mat2 -> Mat2
+add = runFn2 js_add
