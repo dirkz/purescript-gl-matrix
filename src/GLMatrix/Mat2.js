3e460354917e3c9d9b76a7777406fa20c40c5bda
Dirk Zimmermann <me@dirkz.com>
2020-08-14 12:48:04 +0200

    Alphabetical order

--- src/GLMatrix/Mat2.js
+++ src/GLMatrix/Mat2.js
@@ -2,10 +2,6 @@
 
 const m = require("gl-matrix")
 
-exports.js_fromValues = (m00, m01, m10, m11) => {
-    return m.mat2.fromValues(m00, m01, m10, m11)
-}
-
 exports.js_add = (a, b) => {
     let out = m.mat2.create()
     m.mat2.add(out, a, b)
@@ -44,6 +40,10 @@ exports.js_fromScaling = (r) => {
     let out = m.mat2.create()
     m.mat2.fromScaling(out, r)
     return out
+}
+
+exports.js_fromValues = (m00, m01, m10, m11) => {
+    return m.mat2.fromValues(m00, m01, m10, m11)
 }
 
 exports.js_identity = () => {
