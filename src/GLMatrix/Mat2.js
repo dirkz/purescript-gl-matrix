fc5d7bc16d7d3bcb0ee130446e3e5e65918f60c2
Dirk Zimmermann <dirk@pep-project.org>
2020-08-14 11:17:59 +0200

    Mat2.add

--- src/GLMatrix/Mat2.js
+++ src/GLMatrix/Mat2.js
@@ -5,3 +5,9 @@ const m = require("gl-matrix")
 exports.js_fromValues = (m00, m01, m10, m11) => {
     return m.mat2.fromValues(m00, m01, m10, m11)
 }
+
+exports.js_add = (a, b) => {
+    let out = m.mat2.create()
+    m.mat2.add(out, a, b)
+    return out
+}
