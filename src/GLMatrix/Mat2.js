4d8fb46f7ac0aca8617dfc7d3ea20ef020b449dd
Dirk Zimmermann <me@dirkz.com>
2020-08-14 11:57:48 +0200

    More functions

--- src/GLMatrix/Mat2.js
+++ src/GLMatrix/Mat2.js
@@ -29,3 +29,39 @@ exports.js_equals = (a, b) => {
 exports.js_exactEquals = (a, b) => {
     return m.mat2.exactEquals(a, b)
 }
+
+exports.js_frob = (a) => {
+    return m.mat2.frob(a)
+}
+
+exports.js_fromRotation = (r) => {
+    let out = m.mat2.create()
+    m.mat2.fromRotation(out, r)
+    return out
+}
+
+exports.js_fromScaling = (r) => {
+    let out = m.mat2.create()
+    m.mat2.fromScaling(out, r)
+    return out
+}
+
+exports.js_identity = () => {
+    let out = m.mat2.create()
+    m.mat2.identity(out)
+    return out
+}
+
+exports.js_invert = (a) => {
+    let out = m.mat2.create()
+    m.mat2.invert(out, a)
+    return out
+}
+
+exports.js_ldu = (a) => {
+    let l = m.mat2.create()
+    let d = m.mat2.create()
+    let u = m.mat2.create()
+    return m.mat2.ldu(l, d, u, a)
+}
+
--- src/GLMatrix/Mat2.purs
+++ src/GLMatrix/Mat2.purs
@@ -1,7 +1,10 @@
 module GLMatrix.Mat2 where
 
-import Data.Function.Uncurried (Fn2, Fn4, Fn1, runFn1, runFn2, runFn4)
-  
+import Prelude
+import Data.Function.Uncurried (Fn0, Fn1, Fn2, Fn4, runFn0, runFn1, runFn2, runFn4)
+import Data.Tuple.Nested (Tuple3, tuple3)
+import Partial.Unsafe (unsafePartial)
+
 foreign import data Mat2 :: Type
 
 foreign import js_fromValues :: Fn4 Number Number Number Number Mat2
@@ -39,3 +42,41 @@ foreign import js_exactEquals :: Fn2 Mat2 Mat2 Boolean
 -- |Returns whether or not the matrices have exactly the same elements in the same position (when compared with ===)
 exactEquals :: Mat2 -> Mat2 -> Boolean
 exactEquals = runFn2 js_exactEquals
+
+foreign import js_frob :: Fn1 Mat2 Number
+
+-- |Returns Frobenius norm of a mat2
+frob :: Mat2 -> Number
+frob = runFn1 js_frob
+
+foreign import js_fromRotation :: Fn1 Number Mat2
+
+-- |Creates a matrix from a given angle This is equivalent to (but much faster than): mat2.identity(dest); mat2.rotate(dest, dest, rad);
+fromRotation :: Number -> Mat2
+fromRotation = runFn1 js_fromRotation
+
+foreign import js_fromScaling :: Fn1 Number Mat2
+
+-- |Creates a matrix from a vector scaling This is equivalent to (but much faster than): mat2.identity(dest); mat2.scale(dest, dest, vec);
+fromScaling :: Number -> Mat2
+fromScaling = runFn1 js_fromScaling
+
+foreign import js_identity :: Fn0 Mat2
+
+-- |Set a mat2 to the identity matrix
+identity :: Mat2
+identity = runFn0 js_identity
+
+foreign import js_invert :: Fn1 Mat2 Mat2
+
+-- |Inverts a mat2
+invert :: Mat2 -> Mat2
+invert = runFn1 js_invert
+
+foreign import js_ldu :: Fn1 Mat2 (Array Mat2)
+
+array3ToTuple :: forall a. Partial => Array a -> Tuple3 a a a
+array3ToTuple [ a, b, c ] = tuple3 a b c
+
+ldu :: Mat2 -> Tuple3 Mat2 Mat2 Mat2
+ldu m = unsafePartial $ array3ToTuple $ runFn1 js_ldu m
