778bd44b7c35263a6a7617165819e7a4d612d017
Dirk Zimmermann <dirk@pep-project.org>
2020-08-14 11:26:24 +0200

    More Mat2 functions

--- src/GLMatrix/Mat2.js
+++ src/GLMatrix/Mat2.js
@@ -11,3 +11,21 @@ exports.js_add = (a, b) => {
     m.mat2.add(out, a, b)
     return out
 }
+
+exports.js_adjoint = (a) => {
+    let out = m.mat2.create()
+    m.mat2.adjoint(a)
+    return out
+}
+
+exports.js_determinant = (a) => {
+    return m.mat2.determinant(a)
+}
+
+exports.js_equals = (a, b) => {
+    return m.mat2.equals(a, b)
+}
+
+exports.js_exactEquals = (a, b) => {
+    return m.mat2.exactEquals(a, b)
+}
--- src/GLMatrix/Mat2.purs
+++ src/GLMatrix/Mat2.purs
@@ -1,6 +1,6 @@
 module GLMatrix.Mat2 where
 
-import Data.Function.Uncurried (Fn2, Fn4, runFn2, runFn4)
+import Data.Function.Uncurried (Fn2, Fn4, Fn1, runFn1, runFn2, runFn4)
   
 foreign import data Mat2 :: Type
 
@@ -15,3 +15,27 @@ foreign import js_add :: Fn2 Mat2 Mat2 Mat2
 -- |Adds two mat2's
 add :: Mat2 -> Mat2 -> Mat2
 add = runFn2 js_add
+
+foreign import js_adjoint :: Fn1 Mat2 Mat2
+
+-- |Calculates the adjugate of a mat2
+adjoint :: Mat2 -> Mat2
+adjoint = runFn1 js_adjoint
+
+foreign import js_determinant :: Fn1 Mat2 Number
+
+-- |Calculates the determinant of a mat2
+determinant :: Mat2 -> Number
+determinant = runFn1 js_determinant
+
+foreign import js_equals :: Fn2 Mat2 Mat2 Boolean
+
+-- |Returns whether or not the matrices have approximately the same elements in the same position
+equals :: Mat2 -> Mat2 -> Boolean
+equals = runFn2 js_equals
+
+foreign import js_exactEquals :: Fn2 Mat2 Mat2 Boolean
+
+-- |Returns whether or not the matrices have exactly the same elements in the same position (when compared with ===)
+exactEquals :: Mat2 -> Mat2 -> Boolean
+exactEquals = runFn2 js_exactEquals
