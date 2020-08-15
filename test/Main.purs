3f681ec2a132a1c7227b9a20eea66b26d7b98cfd
Dirk Zimmermann <dirk@pep-project.org>
2020-08-15 12:25:15 +0200

    Remove outdated docs

--- test/Main.purs
+++ test/Main.purs
@@ -8,7 +8,6 @@ import Math (pi)
 import Test.QuickCheck (quickCheck, (<?>))
 import Test.TestMat2 as TestMat2
 
--- |Covers `setMatrixArrayType`, `equals` and `toRadian`, so the whole root package
 testToRadian :: Effect Unit
 testToRadian = do
   quickCheck \n -> epsilonEquals (toRadian n) (n * pi / 180.0) <?> "testToRadian " <> show n
