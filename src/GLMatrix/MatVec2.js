ad2b95347b524acb777d167ce41b3c8b303dfbcc
Dirk Zimmermann <dirk@pep-project.org>
2020-08-15 10:05:19 +0200

    Break cyclic dependencies, fix Mat2.fromScaling

---
+++ src/GLMatrix/MatVec2.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const m = require("gl-matrix")
+
+exports.js_transformMat2 = (a, m) => {
+    let out = m.vec2.create()
+    m.vec2.transformMat2(out, a, m)
+    return out
+}
+
+exports.js_fromScaling = (v) => {
+    let out = m.mat2.create()
+    m.mat2.fromScaling(out, r)
+    return out
+}
