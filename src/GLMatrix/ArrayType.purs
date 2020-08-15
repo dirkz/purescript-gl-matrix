8b84df091fb5ebaefe0a8d212940c4903f9fbc64
Dirk Zimmermann <dirk@pep-project.org>
2020-08-15 13:21:08 +0200

    Docs

--- src/GLMatrix/ArrayType.purs
+++ src/GLMatrix/ArrayType.purs
@@ -5,7 +5,9 @@ import Prelude
 import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
 
 -- |Represents the array type used internally by glMatrix,
--- |either Float32Array of an array of numbers.
+-- |which is either Float32Array or an array of numbers.
+-- |All glMatrix types (like vec2, mat2 etc.) are essentially just this type,
+-- |so functionality can be shared.
 foreign import data ArrayType :: Type
 
 foreign import js_numbers :: Fn1 ArrayType (Array Number)
@@ -16,19 +18,19 @@ numbers = runFn1 js_numbers
 
 foreign import js_str :: Fn1 ArrayType String
 
--- |Returns a string representation of a mat2
+-- |Returns a string representation of this array
 str :: ArrayType -> String
 str = runFn1 js_str
 
 foreign import js_epsilonEquals :: Fn2 ArrayType ArrayType Boolean
 
--- |Returns whether or not the matrices have approximately the same elements in the same position
+-- |Returns whether or not the arrays have approximately the same elements in the same position
 epsilonEquals :: ArrayType -> ArrayType -> Boolean
 epsilonEquals = runFn2 js_epsilonEquals
 
 foreign import js_exactEquals :: Fn2 ArrayType ArrayType Boolean
 
--- |Returns whether or not the matrices have exactly the same elements in the same position (when compared with ===)
+-- |Returns whether or not the arrays have exactly the same elements in the same position (when compared with ===)
 exactEquals :: ArrayType -> ArrayType -> Boolean
 exactEquals = runFn2 js_exactEquals
 
