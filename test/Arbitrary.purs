0c117dd4c2f100325a1728470baa20134dcc11bc
Dirk Zimmermann <me@dirkz.com>
2020-08-17 14:05:34 +0200

    Arbitrary for Mat3

--- test/Arbitrary.purs
+++ test/Arbitrary.purs
@@ -3,6 +3,8 @@ module Test.Arbitrary where
 import Prelude
 import GLMatrix.Mat2 (Mat2)
 import GLMatrix.Mat2 as Mat2
+import GLMatrix.Mat3 (Mat3)
+import GLMatrix.Mat3 as Mat3
 import GLMatrix.Vec2 (Vec2)
 import GLMatrix.Vec2 as Vec2
 import Test.QuickCheck (class Arbitrary, arbitrary)
@@ -12,6 +14,12 @@ newtype ArbMat2
 
 instance arbMat2 :: Arbitrary ArbMat2 where
   arbitrary = ArbMat2 <$> (Mat2.fromValues <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary)
+
+newtype ArbMat3
+  = ArbMat3 Mat3
+
+instance arbMat3 :: Arbitrary ArbMat3 where
+  arbitrary = ArbMat3 <$> (Mat3.fromValues <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary)
 
 newtype ArbVec2
   = ArbVec2 Vec2
