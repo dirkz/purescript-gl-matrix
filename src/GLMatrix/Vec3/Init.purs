1f0ee85341558abf058551323ae2177e9db34893
Dirk Zimmermann <dirk@pep-project.org>
2020-08-18 11:29:27 +0200

    Vec3.fromVec2

---
+++ src/GLMatrix/Vec3/Init.purs
@@ -0,0 +1,12 @@
+module GLMatrix.Vec3.Init where
+
+import Prelude
+import GLMatrix.Vec2 as Vec2
+import GLMatrix.Vec3 as Vec3
+import Partial.Unsafe (unsafePartial)
+
+fromVec2 :: Vec2.Vec2 -> Vec3.Vec3
+fromVec2 v = unsafePartial $ Vec3.unsafeFromNumbers $ extendArray $ Vec2.numbers v
+  where
+  extendArray :: Partial => Array Number -> Array Number
+  extendArray [ x, y ] = [ x, y, 0.0 ]
