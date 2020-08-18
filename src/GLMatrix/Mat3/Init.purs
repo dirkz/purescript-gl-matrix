6400afe01b11ac7f2d7d8e48364fe43ca3c5cb3d
Dirk Zimmermann <me@dirkz.com>
2020-08-18 15:47:46 +0200

    Mat3.fromMat4

--- src/GLMatrix/Mat3/Init.purs
+++ src/GLMatrix/Mat3/Init.purs
@@ -1,8 +1,10 @@
 module GLMatrix.Mat3.Init where
 
 import Prelude
+import Data.Function.Uncurried (Fn1, runFn1)
 import GLMatrix.Mat3 (Mat3)
 import GLMatrix.Mat3 as Mat3
+import GLMatrix.Mat4 (Mat4)
 import GLMatrix.Vec3 (Vec3)
 import GLMatrix.Vec3 as Vec3
 import Partial.Unsafe (unsafePartial)
@@ -11,3 +13,8 @@ fromVec3 :: Vec3 -> Vec3 -> Vec3 -> Mat3
 fromVec3 v1 v2 v3 = unsafePartial $ Mat3.unsafeFromNumbers nums
   where
   nums = Vec3.numbers v1 <> (Vec3.numbers v2) <> (Vec3.numbers v3)
+
+foreign import js_fromMat4 :: Fn1 Mat4 Mat3
+
+fromMat4 :: Mat4 -> Mat3
+fromMat4 = runFn1 js_fromMat4
