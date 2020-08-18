bad6471656e8ad9525219fb37b97cb3b864b7e79
Dirk Zimmermann <me@dirkz.com>
2020-08-18 15:09:22 +0200

    Mat2.fromScaling

--- src/GLMatrix/Mat2/Transform.js
+++ src/GLMatrix/Mat2/Transform.js
@@ -2,6 +2,12 @@
 
 const m = require("gl-matrix")
 
+exports.js_fromScaling = (r) => {
+    let out = m.mat2.create()
+    return m.mat2.fromScaling(out, r)
+    return out
+}
+
 exports.js_scale = (a, v) => {
     let out = m.mat2.create()
     m.mat2.scale(out, a, v)
