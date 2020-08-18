5dbd324da79695132a4c0a925986f6a53b2aad15
Dirk Zimmermann <dirk@pep-project.org>
2020-08-18 15:09:22 +0200

    Mat2.fromScaling

--- src/GLMatrix/Mat2/Transform.purs
+++ src/GLMatrix/Mat2/Transform.purs
@@ -1,8 +1,14 @@
 module GLMatrix.Mat2.Transform where
 
-import Data.Function.Uncurried (Fn2, runFn2)
+import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
 import GLMatrix.Mat2 (Mat2)
 import GLMatrix.Vec2 (Vec2)
+
+foreign import js_fromScaling :: Fn1 Vec2 Mat2
+
+-- |Creates a matrix from a vector scaling This is equivalent to (but much faster than): mat2.identity(dest); mat2.scale(dest, dest, vec);
+fromScaling :: Vec2 -> Mat2
+fromScaling = runFn1 js_fromScaling
 
 foreign import js_scale :: Fn2 Mat2 Vec2 Mat2
 
