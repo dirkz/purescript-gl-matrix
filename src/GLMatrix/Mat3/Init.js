901889575418fdef1289c877b090247ccfa3dc69
Dirk Zimmermann <me@dirkz.com>
2020-08-22 15:55:56 +0200

    Put functions in correct module

--- src/GLMatrix/Mat3/Init.js
+++ src/GLMatrix/Mat3/Init.js
@@ -14,6 +14,12 @@ exports.js_fromScaling = (v) => {
     return out
 }
 
+exports.js_fromTranslation = (v) => {
+    let out = m.mat3.create()
+    m.mat3.fromTranslation(out, v)
+    return out
+}
+
 exports.js_normalFromMat4 = (m4) => {
     let out = m.mat3.create()
     m.mat3.normalFromMat4(out, m4)
--- src/GLMatrix/Mat3/Transform.purs
+++ src/GLMatrix/Mat3/Transform.purs
@@ -1,25 +1,11 @@
 module GLMatrix.Mat3.Transform
-  ( fromScaling
-  , fromTranslation
-  , scale
+  ( scale
   , translate
   ) where
 
-import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
+import Data.Function.Uncurried (Fn2, runFn2)
 import GLMatrix.Mat3 (Mat3)
 import GLMatrix.Vec2 (Vec2)
-
-foreign import js_fromScaling :: Fn1 Vec2 Mat3
-
--- |Creates a matrix from a vector scaling This is equivalent to (but much faster than): mat3.identity(dest); mat3.scale(dest, dest, vec);
-fromScaling :: Vec2 -> Mat3
-fromScaling = runFn1 js_fromScaling
-
-foreign import js_fromTranslation :: Fn1 Vec2 Mat3
-
--- |Creates a matrix from a vector translation This is equivalent to (but much faster than): mat3.identity(dest); mat3.translate(dest, dest, vec);
-fromTranslation :: Vec2 -> Mat3
-fromTranslation = runFn1 js_fromScaling
 
 foreign import js_scale :: Fn2 Mat3 Vec2 Mat3
 
