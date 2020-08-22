82bff5edea61adf5dae7c997abc588e14c9251b4
Dirk Zimmermann <dirk@pep-project.org>
2020-08-22 18:58:53 +0200

    Only one mixed package

--- src/GLMatrix/Mat3/Mix.js
+++ src/GLMatrix/Mat3/Mix.js
@@ -25,3 +25,21 @@ exports.js_normalFromMat4 = (m4) => {
     m.mat3.normalFromMat4(out, m4)
     return out
 }
+
+exports.js_scale = (m3, v2) => {
+    let out = m.mat3.create()
+    m.mat3.scale(out, m3, v2)
+    return out
+}
+
+exports.js_translate = (m3, v2) => {
+    let out = m.mat3.create()
+    m.mat3.translate(out, m3, v2)
+    return out
+}
+
+exports.js_normalFromMat4 = (a) => {
+    let out = m.mat3.create()
+    m.mat3.normalFromMat4(out, a)
+    return out
+}
