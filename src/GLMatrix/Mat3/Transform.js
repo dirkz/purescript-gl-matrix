845088ad2677a3b2dda9735e27856343bf1c3aec
Dirk Zimmermann <dirk@pep-project.org>
2020-08-22 15:37:45 +0200

    Mat3.normalFromMat4

--- src/GLMatrix/Mat3/Transform.js
+++ src/GLMatrix/Mat3/Transform.js
@@ -13,3 +13,9 @@ exports.js_fromTranslation = (v) => {
     m.mat3.fromTranslation(out, v)
     return out
 }
+
+exports.js_normalFromMat4 = (m4) => {
+    let out = m.mat3.create()
+    m.mat3.normalFromMat4(out, m4)
+    return out
+}
