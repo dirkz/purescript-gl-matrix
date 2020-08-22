68d46b256d49bdc4f39627c91f7a31ecf6756578
Dirk Zimmermann <me@dirkz.com>
2020-08-22 19:00:02 +0200

    Remove empty files

--- src/GLMatrix/Mat3/Transform.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const m = require("gl-matrix")
-
--- src/GLMatrix/Mat3/Transform.purs
+++
@@ -1,6 +0,0 @@
-module GLMatrix.Mat3.Transform where
-
-import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
-import GLMatrix.Mat3 (Mat3)
-import GLMatrix.Mat4 (Mat4)
-import GLMatrix.Vec2 (Vec2)
