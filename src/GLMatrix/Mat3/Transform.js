cdfda805630481d5b5195121ae15d4664efb295c
Dirk Zimmermann <dirk@pep-project.org>
2020-08-22 14:43:58 +0200

    Mat3.fromTranslation

--- src/GLMatrix/Mat3/Transform.js
+++ src/GLMatrix/Mat3/Transform.js
@@ -7,3 +7,9 @@ exports.js_fromScaling = (v) => {
     m.mat3.fromScaling(out, v)
     return out
 }
+
+exports.js_fromTranslation = (v) => {
+    let out = m.mat3.create()
+    m.mat3.fromTranslation(out, v)
+    return out
+}
--- src/GLMatrix/Mat3/Transform.purs
+++ src/GLMatrix/Mat3/Transform.purs
@@ -9,3 +9,9 @@ foreign import js_fromScaling :: Fn1 Vec2 Mat3
 -- |Creates a matrix from a vector scaling This is equivalent to (but much faster than): mat3.identity(dest); mat3.scale(dest, dest, vec);
 fromScaling :: Vec2 -> Mat3
 fromScaling = runFn1 js_fromScaling
+
+foreign import js_fromTranslation :: Fn1 Vec2 Mat3
+
+-- |Creates a matrix from a vector translation This is equivalent to (but much faster than): mat3.identity(dest); mat3.translate(dest, dest, vec);
+fromTranslation :: Vec2 -> Mat3
+fromTranslation = runFn1 js_fromScaling
