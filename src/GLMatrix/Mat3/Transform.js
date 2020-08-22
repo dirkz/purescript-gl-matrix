d00a6c707a7595f3f83d2a8bce9a2ec4f895f3cd
Dirk Zimmermann <dirk@pep-project.org>
2020-08-22 15:47:23 +0200

    Mat3.scale

--- src/GLMatrix/Mat3/Transform.js
+++ src/GLMatrix/Mat3/Transform.js
@@ -13,3 +13,9 @@ exports.js_fromTranslation = (v) => {
     m.mat3.fromTranslation(out, v)
     return out
 }
+
+exports.js_scale = (m3, v2) => {
+    let out = m.mat3.create()
+    m.mat3.scale(out, m3, v2)
+    return out
+}
--- src/GLMatrix/Mat3/Transform.purs
+++ src/GLMatrix/Mat3/Transform.purs
@@ -1,9 +1,10 @@
 module GLMatrix.Mat3.Transform
   ( fromScaling
   , fromTranslation
+  , scale
   ) where
 
-import Data.Function.Uncurried (Fn1, runFn1)
+import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
 import GLMatrix.Mat3 (Mat3)
 import GLMatrix.Vec2 (Vec2)
 
@@ -18,3 +19,9 @@ foreign import js_fromTranslation :: Fn1 Vec2 Mat3
 -- |Creates a matrix from a vector translation This is equivalent to (but much faster than): mat3.identity(dest); mat3.translate(dest, dest, vec);
 fromTranslation :: Vec2 -> Mat3
 fromTranslation = runFn1 js_fromScaling
+
+foreign import js_scale :: Fn2 Mat3 Vec2 Mat3
+
+-- |Scales the mat3 by the dimensions in the given vec2
+scale :: Mat3 -> Vec2 -> Mat3
+scale = runFn2 js_scale
