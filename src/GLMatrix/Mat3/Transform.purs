71650592fb4cc55966abf3b63a988b4b7a8c2fb7
Dirk Zimmermann <me@dirkz.com>
2020-08-22 15:37:45 +0200

    Mat3.normalFromMat4

--- src/GLMatrix/Mat3/Transform.purs
+++ src/GLMatrix/Mat3/Transform.purs
@@ -1,7 +1,11 @@
-module GLMatrix.Mat3.Transform (fromScaling) where
+module GLMatrix.Mat3.Transform
+  ( fromScaling
+  , normalFromMat4
+  ) where
 
 import Data.Function.Uncurried (Fn1, runFn1)
 import GLMatrix.Mat3 (Mat3)
+import GLMatrix.Mat4 (Mat4)
 import GLMatrix.Vec2 (Vec2)
 
 foreign import js_fromScaling :: Fn1 Vec2 Mat3
@@ -15,3 +19,9 @@ foreign import js_fromTranslation :: Fn1 Vec2 Mat3
 -- |Creates a matrix from a vector translation This is equivalent to (but much faster than): mat3.identity(dest); mat3.translate(dest, dest, vec);
 fromTranslation :: Vec2 -> Mat3
 fromTranslation = runFn1 js_fromScaling
+
+foreign import js_normalFromMat4 :: Fn1 Mat4 Mat3
+
+-- |Calculates a 3x3 normal matrix (transpose inverse) from the 4x4 matrix
+normalFromMat4 :: Mat4 -> Mat3
+normalFromMat4 = runFn1 js_normalFromMat4
