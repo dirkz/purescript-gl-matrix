9a6df79346e68b7fe570f13b7cfb2a4598c04332
Dirk Zimmermann <me@dirkz.com>
2020-08-22 18:58:53 +0200

    Only one mixed package

--- src/GLMatrix/Mat3/Transform.purs
+++ src/GLMatrix/Mat3/Transform.purs
@@ -1,28 +1,6 @@
-module GLMatrix.Mat3.Transform
-  ( normalFromMat4
-  , scale
-  , translate
-  ) where
+module GLMatrix.Mat3.Transform where
 
 import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
 import GLMatrix.Mat3 (Mat3)
 import GLMatrix.Mat4 (Mat4)
 import GLMatrix.Vec2 (Vec2)
-
-foreign import js_scale :: Fn2 Mat3 Vec2 Mat3
-
--- |Scales the mat3 by the dimensions in the given vec2
-scale :: Mat3 -> Vec2 -> Mat3
-scale = runFn2 js_scale
-
-foreign import js_translate :: Fn2 Mat3 Vec2 Mat3
-
--- |Translate a mat3 by the given vector
-translate :: Mat3 -> Vec2 -> Mat3
-translate = runFn2 js_translate
-
-foreign import js_normalFromMat4 :: Fn1 Mat4 Mat3
-
--- |Calculates a 3x3 normal matrix (transpose inverse) from the 4x4 matrix
-normalFromMat4 :: Mat4 -> Mat3
-normalFromMat4 = runFn1 js_normalFromMat4
