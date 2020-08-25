5368202f4b57ed3a798829121b7a10815454ac72
Dirk Zimmermann <dirk@pep-project.org>
2020-08-25 09:56:01 +0200

    Mat2d.fromVec2

--- src/GLMatrix/Mat2d/Mix.purs
+++ src/GLMatrix/Mat2d/Mix.purs
@@ -1,8 +1,11 @@
 module GLMatrix.Mat2d.Mix where
 
+import Prelude
 import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
-import GLMatrix.Mat2d (Mat2d)
+import GLMatrix.Mat2d (Mat2d, unsafeFromNumbers)
 import GLMatrix.Vec2 (Vec2)
+import GLMatrix.Vec2 as Vec2
+import Partial.Unsafe (unsafePartial)
 
 foreign import js_fromScaling :: Fn1 Vec2 Mat2d
 
@@ -27,3 +30,8 @@ foreign import js_translate :: Fn2 Mat2d Vec2 Mat2d
 -- |Translate a mat2d by the given vector
 translate :: Mat2d -> Vec2 -> Mat2d
 translate = runFn2 js_translate
+
+fromVec2 :: Vec2 -> Vec2 -> Mat2d
+fromVec2 v1 v2 = unsafePartial $ unsafeFromNumbers nums
+  where
+  nums = Vec2.numbers v1 <> (Vec2.numbers v2)
