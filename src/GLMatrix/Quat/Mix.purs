3f07e49621e1766ee8bc24eb102922a3b64b5d6e
Dirk Zimmermann <dirk@pep-project.org>
2020-08-25 10:51:58 +0200

    Quat.sqlerp

--- src/GLMatrix/Quat/Mix.purs
+++ src/GLMatrix/Quat/Mix.purs
@@ -1,6 +1,6 @@
 module GLMatrix.Quat.Mix where
 
-import Data.Function.Uncurried (Fn2, Fn3, runFn2, runFn3)
+import Data.Function.Uncurried (Fn2, Fn3, Fn4, runFn2, runFn3, runFn4)
 import GLMatrix.Quat (Quat)
 import GLMatrix.Vec3 (Vec3)
 
@@ -15,3 +15,9 @@ foreign import js_setAxes :: Fn3 Vec3 Vec3 Vec3 Quat
 -- |Sets the specified quaternion with values corresponding to the given axes. Each axis is a vec3 and is expected to be unit length and perpendicular to all other specified axes.
 setAxes :: Vec3 -> Vec3 -> Vec3 -> Quat
 setAxes = runFn3 js_setAxes
+
+foreign import js_sqlerp :: Fn4 Number Number Number Number Quat
+
+-- |Sets the specified quaternion with values corresponding to the given axes. Each axis is a vec3 and is expected to be unit length and perpendicular to all other specified axes.
+sqlerp :: Number -> Number -> Number -> Number -> Quat
+sqlerp = runFn4 js_sqlerp
