29e3a9c07663306adb058e2005014f25848e5372
Dirk Zimmermann <me@dirkz.com>
2020-08-25 10:47:19 +0200

    Change rotationTo

--- src/GLMatrix/Quat/Mix.js
+++ src/GLMatrix/Quat/Mix.js
@@ -3,5 +3,7 @@
 const m = require("gl-matrix")
 
 exports.js_rotationTo = (a, b) => {
-    return m.quat.rotationTo(a, b)
+    let out = m.quat.create()
+    m.quat.rotationTo(out, a, b)
+    return out
 }
