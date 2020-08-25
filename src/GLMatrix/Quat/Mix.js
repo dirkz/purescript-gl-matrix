60d6db09072f07dbe8888c6efd1742d3cef34709
Dirk Zimmermann <dirk@pep-project.org>
2020-08-25 10:49:29 +0200

    Quat.setAxes

--- src/GLMatrix/Quat/Mix.js
+++ src/GLMatrix/Quat/Mix.js
@@ -7,3 +7,9 @@ exports.js_rotationTo = (a, b) => {
     m.quat.rotationTo(out, a, b)
     return out
 }
+
+exports.js_setAxes = (view, right, up) => {
+    let out = m.quat.create()
+    m.quat.setAxes(out, view, right, up)
+    return out
+}
--- src/GLMatrix/Quat/Mix.purs
+++ src/GLMatrix/Quat/Mix.purs
@@ -1,6 +1,6 @@
 module GLMatrix.Quat.Mix where
 
-import Data.Function.Uncurried (Fn2, runFn2)
+import Data.Function.Uncurried (Fn2, Fn3, runFn2, runFn3)
 import GLMatrix.Quat (Quat)
 import GLMatrix.Vec3 (Vec3)
 
@@ -9,3 +9,9 @@ foreign import js_rotationTo :: Fn2 Vec3 Vec3 Quat
 -- |Sets a quaternion to represent the shortest rotation from one vector to another. Both vectors are assumed to be unit length.
 rotationTo :: Vec3 -> Vec3 -> Quat
 rotationTo = runFn2 js_rotationTo
+
+foreign import js_setAxes :: Fn3 Vec3 Vec3 Vec3 Quat
+
+-- |Sets the specified quaternion with values corresponding to the given axes. Each axis is a vec3 and is expected to be unit length and perpendicular to all other specified axes.
+setAxes :: Vec3 -> Vec3 -> Vec3 -> Quat
+setAxes = runFn3 js_setAxes
