f77019d57c1a77b039af1c13db3fa10d747b62ac
Dirk Zimmermann <dirk@pep-project.org>
2020-08-25 09:52:24 +0200

    Arbitrary Mat2d

--- test/Arbitrary.purs
+++ test/Arbitrary.purs
@@ -3,6 +3,8 @@ module Test.Arbitrary where
 import Prelude
 import GLMatrix.Mat2 (Mat2)
 import GLMatrix.Mat2 as Mat2
+import GLMatrix.Mat2d (Mat2d)
+import GLMatrix.Mat2d as Mat2d
 import GLMatrix.Mat3 (Mat3)
 import GLMatrix.Mat3 as Mat3
 import GLMatrix.Mat4 (FieldOfView, Mat4, fieldOfView)
@@ -20,6 +22,12 @@ newtype ArbMat2
 
 instance arbMat2 :: Arbitrary ArbMat2 where
   arbitrary = ArbMat2 <$> (Mat2.fromValues <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary)
+
+newtype ArbMat2d
+  = ArbMat2d Mat2d
+
+instance arbMat2d :: Arbitrary ArbMat2d where
+  arbitrary = ArbMat2d <$> (Mat2d.fromValues <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary)
 
 newtype ArbMat3
   = ArbMat3 Mat3
