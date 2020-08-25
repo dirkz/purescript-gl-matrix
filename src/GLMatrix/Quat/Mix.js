3cf3e9dfea14decf1e49e54c5f6ef505c76687d7
Dirk Zimmermann <me@dirkz.com>
2020-08-25 10:45:42 +0200

    Quat.rotationTo

--- src/GLMatrix/Quat/Mix.js
+++ src/GLMatrix/Quat/Mix.js
@@ -2,3 +2,6 @@
 
 const m = require("gl-matrix")
 
+exports.js_rotationTo = (a, b) => {
+    return m.quat.rotationTo(a, b)
+}
--- src/GLMatrix/Quat/Mix.purs
+++ src/GLMatrix/Quat/Mix.purs
@@ -1,2 +1,11 @@
 module GLMatrix.Quat.Mix where
 
+import Data.Function.Uncurried (Fn2, runFn2)
+import GLMatrix.Quat (Quat)
+import GLMatrix.Vec3 (Vec3)
+
+foreign import js_rotationTo :: Fn2 Vec3 Vec3 Quat
+
+-- |Sets a quaternion to represent the shortest rotation from one vector to another. Both vectors are assumed to be unit length.
+rotationTo :: Vec3 -> Vec3 -> Quat
+rotationTo = runFn2 js_rotationTo
