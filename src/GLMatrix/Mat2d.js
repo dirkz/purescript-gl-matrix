049d827189675f6766540a973b7f431aa01d5072
Dirk Zimmermann <me@dirkz.com>
2020-08-25 09:32:13 +0200

    Add Mat2d

---
+++ src/GLMatrix/Mat2d.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const m = require("gl-matrix")
+
+exports.js_add = (a, b) => {
+    let out = m.mat2d.create()
+    m.mat2d.add(out, a, b)
+    return out
+}
+
+exports.js_adjoint = (a) => {
+    let out = m.mat2d.create()
+    m.mat2d.adjoint(out, a)
+    return out
+}
+
+exports.js_determinant = (a) => {
+    return m.mat2d.determinant(a)
+}
+
+exports.js_equals = (a, b) => {
+    return m.mat2d.equals(a, b)
+}
+
+exports.js_exactEquals = (a, b) => {
+    return m.mat2d.exactEquals(a, b)
+}
+
+exports.js_frob = (a) => {
+    return m.mat2d.frob(a)
+}
+
+exports.js_fromRotation = (r) => {
+    let out = m.mat2d.create()
+    m.mat2d.fromRotation(out, r)
+    return out
+}
+
+exports.js_fromValues = (m00, m01, m10, m11) => {
+    return m.mat2d.fromValues(m00, m01, m10, m11)
+}
+
+exports.js_identity = () => {
+    return m.mat2d.create()
+}
+
+exports.js_invert = (a) => {
+    let out = m.mat2d.create()
+    m.mat2d.invert(out, a)
+    return out
+}
+
+exports.js_ldu = (a) => {
+    let l = m.mat2d.create()
+    let d = m.mat2d.create()
+    let u = m.mat2d.create()
+    return m.mat2d.LDU(l, d, u, a)
+}
+
+exports.js_multiply = (a, b) => {
+    let out = m.mat2d.create()
+    m.mat2d.multiply(out, a, b)
+    return out
+}
+
+exports.js_multiplyScalar = (a, b) => {
+    let out = m.mat2d.create()
+    m.mat2d.multiplyScalar(out, a, b)
+    return out
+}
+
+exports.js_multiplyScalarAndAdd = (a, b, scale) => {
+    let out = m.mat2d.create()
+    m.mat2d.multiplyScalarAndAdd(out, a, b, scale)
+    return out
+}
+
+exports.js_rotate = (a, r) => {
+    let out = m.mat2d.create()
+    m.mat2d.rotate(out, a, r)
+    return out
+}
+
+exports.js_str = (a) => {
+    return m.mat2d.str(a)
+}
+
+exports.js_subtract = (a, b) => {
+    let out = m.mat2d.create()
+    m.mat2d.subtract(out, a, b)
+    return out
+}
+
+exports.js_transpose = (a) => {
+    let out = m.mat2d.create()
+    m.mat2d.transpose(out, a)
+    return out
+}
+
+exports.js_numbers = (ar) => {
+    return Array.prototype.slice.call(ar)
+}
---
+++ src/GLMatrix/Mat2d.purs
@@ -0,0 +1,175 @@
+module GLMatrix.Mat2d
+  ( Mat2d
+  , LDU
+  , add
+  , adjoint
+  , determinant
+  , equals
+  , frob
+  , fromRotation
+  , fromValues
+  , identity
+  , invert
+  , ldu
+  , multiply
+  , multiplyScalar
+  , multiplyScalarAndAdd
+  , rotate
+  , subtract
+  , transpose
+  , numbers
+  , map
+  , unsafeFromNumbers
+  , zipWith
+  , slice
+  ) where
+
+import Data.Array as Array
+import Data.Function.Uncurried (Fn0, Fn1, Fn2, Fn3, Fn4, runFn0, runFn1, runFn2, runFn3, runFn4)
+import Partial.Unsafe (unsafePartial)
+import Prelude (($))
+import Prelude as Prelude
+
+foreign import data Mat2d :: Type
+
+foreign import js_add :: Fn2 Mat2d Mat2d Mat2d
+
+-- |Adds two Mat2d's
+add :: Mat2d -> Mat2d -> Mat2d
+add = runFn2 js_add
+
+foreign import js_adjoint :: Fn1 Mat2d Mat2d
+
+-- |Calculates the adjugate of a Mat2d
+adjoint :: Mat2d -> Mat2d
+adjoint = runFn1 js_adjoint
+
+foreign import js_determinant :: Fn1 Mat2d Number
+
+-- |Calculates the determinant of a Mat2d
+determinant :: Mat2d -> Number
+determinant = runFn1 js_determinant
+
+foreign import js_equals :: Fn2 Mat2d Mat2d Boolean
+
+-- |Returns whether or not the matrices have approximately the same elements in the same position
+equals :: Mat2d -> Mat2d -> Boolean
+equals = runFn2 js_equals
+
+foreign import js_exactEquals :: Fn2 Mat2d Mat2d Boolean
+
+-- |Returns whether or not the matrices have exactly the same elements in the same position (when compared with ===)
+exactEquals :: Mat2d -> Mat2d -> Boolean
+exactEquals = runFn2 js_exactEquals
+
+foreign import js_frob :: Fn1 Mat2d Number
+
+-- |Returns Frobenius norm of a Mat2d
+frob :: Mat2d -> Number
+frob = runFn1 js_frob
+
+foreign import js_fromRotation :: Fn1 Number Mat2d
+
+-- |Creates a matrix from a given angle This is equivalent to (but much faster than): Mat2d.identity(dest); Mat2d.rotate(dest, dest, rad);
+fromRotation :: Number -> Mat2d
+fromRotation = runFn1 js_fromRotation
+
+foreign import js_fromValues :: Fn4 Number Number Number Number Mat2d
+
+-- |Create a new Mat2d with the given values
+fromValues :: Number -> Number -> Number -> Number -> Mat2d
+fromValues = runFn4 js_fromValues
+
+foreign import js_identity :: Fn0 Mat2d
+
+-- |Set a Mat2d to the identity matrix
+identity :: Mat2d
+identity = runFn0 js_identity
+
+foreign import js_invert :: Fn1 Mat2d Mat2d
+
+-- |Inverts a Mat2d
+invert :: Mat2d -> Mat2d
+invert = runFn1 js_invert
+
+type LDU
+  = { l :: Mat2d, d :: Mat2d, u :: Mat2d }
+
+foreign import js_ldu :: Fn1 Mat2d (Array Mat2d)
+
+-- |Returns L, D and U matrices (Lower triangular, Diagonal and Upper triangular) by factorizing the input matrix
+ldu :: Mat2d -> LDU
+ldu m = unsafePartial $ array3ToRecord $ runFn1 js_ldu m
+  where
+  array3ToRecord :: Partial => Array Mat2d -> LDU
+  array3ToRecord [ a, b, c ] = { l: a, d: b, u: c }
+
+foreign import js_multiply :: Fn2 Mat2d Mat2d Mat2d
+
+-- |Multiplies two Mat2d's
+multiply :: Mat2d -> Mat2d -> Mat2d
+multiply = runFn2 js_multiply
+
+foreign import js_multiplyScalar :: Fn2 Mat2d Number Mat2d
+
+-- |Multiply each element of the matrix by a scalar
+multiplyScalar :: Mat2d -> Number -> Mat2d
+multiplyScalar = runFn2 js_multiplyScalar
+
+foreign import js_multiplyScalarAndAdd :: Fn3 Mat2d Mat2d Number Mat2d
+
+-- |Adds two Mat2d's after multiplying each element of the second operand by a scalar value
+multiplyScalarAndAdd :: Mat2d -> Mat2d -> Number -> Mat2d
+multiplyScalarAndAdd = runFn3 js_multiplyScalarAndAdd
+
+foreign import js_rotate :: Fn2 Mat2d Number Mat2d
+
+-- |Rotates a Mat2d by the given angle
+rotate :: Mat2d -> Number -> Mat2d
+rotate = runFn2 js_rotate
+
+foreign import js_str :: Fn1 Mat2d String
+
+-- |Returns a string representation of a Mat2d
+str :: Mat2d -> String
+str = runFn1 js_str
+
+foreign import js_subtract :: Fn2 Mat2d Mat2d Mat2d
+
+-- |Subtracts matrix b from matrix a
+subtract :: Mat2d -> Mat2d -> Mat2d
+subtract = runFn2 js_subtract
+
+foreign import js_transpose :: Fn1 Mat2d Mat2d
+
+-- |Transpose the values of a Mat2d
+transpose :: Mat2d -> Mat2d
+transpose = runFn1 js_transpose
+
+foreign import js_numbers :: Fn1 Mat2d (Array Number)
+
+-- |Extract a number array
+numbers :: Mat2d -> Array Number
+numbers = runFn1 js_numbers
+
+-- |Create a matrix from an array produced by `numbers`.
+unsafeFromNumbers :: Partial => Array Number -> Mat2d
+unsafeFromNumbers [ m00, m01, m10, m11 ] = fromValues m00 m01 m10 m11
+
+instance showMat2d :: Prelude.Show Mat2d where
+  show = str
+
+instance eqMat2d :: Prelude.Eq Mat2d where
+  eq = exactEquals
+
+-- |Map a function from `Number` to `Number` over it.
+-- |Note: Since this is not a general container, it cannot be a `Functor`.
+map :: (Number -> Number) -> Mat2d -> Mat2d
+map fn v = unsafePartial $ unsafeFromNumbers $ Prelude.map fn $ numbers v
+
+zipWith :: (Number -> Number -> Number) -> Mat2d -> Mat2d -> Mat2d
+zipWith fn v1 v2 = unsafePartial $ unsafeFromNumbers $ Array.zipWith fn (numbers v1) (numbers v2)
+
+-- |Like `Array.slice`
+slice :: Int -> Int -> Mat2d -> Array Number
+slice a b m = Array.slice a b $ numbers m
