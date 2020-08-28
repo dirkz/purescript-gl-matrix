d098cb5d206a95b5c8a0a5819bbd510f573eea36
Dirk Zimmermann <dirk@pep-project.org>
2020-08-28 13:47:57 +0200

    It's Mat3.fromMat2d

--- src/GLMatrix/Mat2/Mix.js
+++ src/GLMatrix/Mat2/Mix.js
@@ -2,12 +2,6 @@
 
 const m = require("gl-matrix")
 
-exports.js_fromMat2d = (m2) => {
-    let out = m.mat2.create()
-    return m.mat2.fromMat2d(out, m2)
-    return out
-}
-
 exports.js_fromScaling = (r) => {
     let out = m.mat2.create()
     return m.mat2.fromScaling(out, r)
--- src/GLMatrix/Mat3/Mix.js
+++ src/GLMatrix/Mat3/Mix.js
@@ -2,6 +2,12 @@
 
 const m = require("gl-matrix")
 
+exports.js_fromMat2d = (m2) => {
+    let out = m.mat2.create()
+    return m.mat2.fromMat2d(out, m2)
+    return out
+}
+
 exports.js_fromMat4 = (mat) => {
     let out = m.mat3.create()
     m.mat3.fromMat4(out, mat)
