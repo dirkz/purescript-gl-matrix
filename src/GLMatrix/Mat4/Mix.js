a0f79f5e3bb845cd6cc7a6096a06d0dda85db971
Dirk Zimmermann <dirk@pep-project.org>
2020-08-29 08:11:05 +0200

    Mat4.fromRotationTranslation

--- src/GLMatrix/Mat4/Mix.js
+++ src/GLMatrix/Mat4/Mix.js
@@ -7,3 +7,9 @@ exports.js_fromQuat = (q) => {
     return m.mat4.fromQuat(out, q)
     return out
 }
+
+exports.js_fromRotationTranslation = (q, v) => {
+    let out = m.mat4.create()
+    return m.mat4.fromRotationTranslation(out, q, v)
+    return out
+}
--- src/GLMatrix/Mat4/Mix.purs
+++ src/GLMatrix/Mat4/Mix.purs
@@ -1,13 +1,15 @@
 module GLMatrix.Mat4.Mix
   ( fromQuat
   , fromVec4
+  , fromRotationTranslation
   ) where
 
 import Prelude
 
-import Data.Function.Uncurried (Fn1, runFn1)
+import Data.Function.Uncurried (Fn1, Fn2, runFn1, runFn2)
 import GLMatrix.Mat4 (Mat4, unsafeFromNumbers)
 import GLMatrix.Quat (Quat)
+import GLMatrix.Vec3 (Vec3)
 import GLMatrix.Vec4 (Vec4)
 import GLMatrix.Vec4 as Vec4
 import Partial.Unsafe (unsafePartial)
@@ -23,3 +25,8 @@ foreign import js_fromQuat :: Fn1 Quat Mat4
 fromQuat :: Quat -> Mat4
 fromQuat = runFn1 js_fromQuat
 
+foreign import js_fromRotationTranslation :: Fn2 Quat Vec3 Mat4
+
+-- |Creates a matrix from a quaternion rotation and vector translation This is equivalent to (but much faster than): mat4.identity(dest); mat4.translate(dest, vec); let quatMat = mat4.create(); quat4.toMat4(quat, quatMat); mat4.multiply(dest, quatMat);
+fromRotationTranslation :: Quat -> Vec3 -> Mat4
+fromRotationTranslation = runFn2 js_fromRotationTranslation
