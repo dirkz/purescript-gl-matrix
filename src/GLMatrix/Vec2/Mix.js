b4cc827986f2cb60784e6ca087672d44f2a8a820
Dirk Zimmermann <me@dirkz.com>
2020-08-29 10:12:20 +0200

    Vec2.transformMatX

--- src/GLMatrix/Vec2/Mix.js
+++ src/GLMatrix/Vec2/Mix.js
@@ -6,3 +6,18 @@ exports.js_transformMat2 = (a, mat) => {
     let out = m.vec2.create()
     return m.vec2.transformMat2(out, a, mat)
 }
+
+exports.js_transformMat2d = (a, mat) => {
+    let out = m.vec2.create()
+    return m.vec2.transformMat2d(out, a, mat)
+}
+
+exports.js_transformMat3 = (a, mat) => {
+    let out = m.vec2.create()
+    return m.vec2.transformMat3(out, a, mat)
+}
+
+exports.js_transformMat4 = (a, mat) => {
+    let out = m.vec2.create()
+    return m.vec2.transformMat4(out, a, mat)
+}
--- src/GLMatrix/Vec2/Mix.purs
+++ src/GLMatrix/Vec2/Mix.purs
@@ -1,7 +1,15 @@
-module GLMatrix.Vec2.Mix where
+module GLMatrix.Vec2.Mix
+  ( transformMat2
+  , transformMat2d
+  , transformMat3
+  , transformMat4
+  ) where
 
 import Data.Function.Uncurried (Fn2, runFn2)
 import GLMatrix.Mat2 (Mat2)
+import GLMatrix.Mat2d (Mat2d)
+import GLMatrix.Mat3 (Mat3)
+import GLMatrix.Mat4 (Mat4)
 import GLMatrix.Vec2 (Vec2)
 
 foreign import js_transformMat2 :: Fn2 Vec2 Mat2 Vec2
@@ -9,3 +17,21 @@ foreign import js_transformMat2 :: Fn2 Vec2 Mat2 Vec2
 -- |Transforms the vec2 with a mat2
 transformMat2 :: Vec2 -> Mat2 -> Vec2
 transformMat2 = runFn2 js_transformMat2
+
+foreign import js_transformMat2d :: Fn2 Vec2 Mat2d Vec2
+
+-- |Transforms the vec2 with a mat2d
+transformMat2d :: Vec2 -> Mat2d -> Vec2
+transformMat2d = runFn2 js_transformMat2d
+
+foreign import js_transformMat3 :: Fn2 Vec2 Mat3 Vec2
+
+-- |Transforms the vec2 with a mat3
+transformMat3 :: Vec2 -> Mat3 -> Vec2
+transformMat3 = runFn2 js_transformMat3
+
+foreign import js_transformMat4 :: Fn2 Vec2 Mat4 Vec2
+
+-- |Transforms the vec2 with a mat4
+transformMat4 :: Vec2 -> Mat4 -> Vec2
+transformMat4 = runFn2 js_transformMat4
